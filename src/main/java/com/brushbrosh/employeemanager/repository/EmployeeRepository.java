80c42846ea46d88da4c260bf11ba45f6593e7572
Hector Martinez <hector.martinez.cred@outlook.com>
2022-08-19 13:40:01 -0500

    Exception Handling

--- src/main/java/com/brushbrosh/employeemanager/repository/EmployeeRepository.java
+++ src/main/java/com/brushbrosh/employeemanager/repository/EmployeeRepository.java
@@ -12,6 +12,6 @@ public interface EmployeeRepository extends JpaRepository<Employee, Long>{
 	
 	void deleteEmployeeById(Long id);
 
-	Optional findEmployeeById(Long id);
+	Optional<Employee> findEmployeeById(Long id);
 
 }
