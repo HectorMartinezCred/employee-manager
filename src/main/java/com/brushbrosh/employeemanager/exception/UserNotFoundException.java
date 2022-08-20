7fa1ec2ab18062b1d9f16f9c5563795e07c6809d
Hector Martinez <hector.martinez.cred@outlook.com>
2022-08-20 11:21:24 -0500

    create EmployteeControler.java
    Exposing the API

--- src/main/java/com/brushbrosh/employeemanager/exception/UserNotFoundException.java
+++ src/main/java/com/brushbrosh/employeemanager/exception/UserNotFoundException.java
@@ -2,6 +2,11 @@ package com.brushbrosh.employeemanager.exception;
 
 public class UserNotFoundException extends RuntimeException{
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	public UserNotFoundException(String message) {
 		super(message);
 	}
